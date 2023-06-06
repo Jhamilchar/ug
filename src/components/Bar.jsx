35aa5217bc65935a23eccbb3d8755d5233c14b18
Jhamil <jhamil.arroyo@gmail.com>
2023-06-06 12:50:16 -0500

    test

---
+++ src/components/Bar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "../styles/bar_styles.css";
+
+export const Bar = () => {
+  const [activeLines, setActiveLines] = useState(false);
+
+  const animateBars = () => {
+    setActiveLines(!activeLines);
+
+  };
+
+  return (
+    <div
+      className={`bars__menu ${activeLines ? "active" : ""}`}
+      onClick={animateBars}
+    >
+      <span
+        className={`line1__bars-menu ${
+          activeLines ? "activeline1__bars-menu" : ""
+        }`}
+      ></span>
+      <span
+        className={`line2__bars-menu ${
+          activeLines ? "activeline2__bars-menu" : ""
+        }`}
+      ></span>
+      <span
+        className={`line3__bars-menu ${
+          activeLines ? "activeline3__bars-menu" : ""
+        }`}
+      ></span>
+    </div>
+  );
+};
---
+++ src/functions/bar.js
@@ -0,0 +1,1 @@
+document.querySelector('.bars__menu')
