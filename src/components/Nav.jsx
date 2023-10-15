e63748a96ba4930e7450257e04f7791984c815ca
Jhamilchar <jhamil.arroyo.a@gmail.com>
2023-10-15 02:12:51 -0500

    pre 5

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -44,11 +44,43 @@ const Nav = () => {
         {showImage && (
           <img src={logoAside} alt="Imagen" className="side-image" />
         )}
-        <ul className={isMenuOpen ? "menu-open" : "menu-closed"}>
-          <li>Elemento 1</li>
-          <li>Elemento 2</li>
-          <li>Elemento 3</li>
-        </ul>
+        <div className={isMenuOpen ? "menu-open" : "menu-closed"}>
+          <div className="container-menu_show">
+            <h2 className="top-text">Inicia tu Viaje en AWS</h2>
+            <p className="desc-text">
+              Descubre un mundo de oportunidades en la nube y únete a nuestra
+              comunidad para aprender, compartir y crecer juntos
+            </p>
+          </div>
+          <ul className="show-menu_wrapper">
+            <li>
+              <a className="show-menu_items" href="">
+                Home
+              </a>
+            </li>
+            <li>
+              <a className="show-menu_items" href="">
+                Acerca De
+              </a>
+            </li>
+            <li>
+              <a className="show-menu_items" href="">
+                Objetivos
+              </a>
+            </li>
+            <li>
+              <a className="show-menu_items" href="">
+                Faqs
+              </a>
+            </li>
+            <li>
+              <a className="show-menu_items" href="">
+                Contact
+              </a>
+            </li>
+          </ul>
+          <li>Pendiente</li>
+        </div>
         <div className="animate__animated animate__slideInUp animate__delay-.8s">
           <a
             className="conf-link"
@@ -63,5 +95,4 @@ const Nav = () => {
   );
 };
 
-// Resto del código CSS y exportación del componente
 export default Nav
