339da718b7ed84df16d3e4c414035aaba89bf2d6
Jhamil <jhamil.arroyo.a@gmail.com>
2024-01-09 10:30:27 -0600

    Change layer

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import '../styles/about-section.css'
 
 export const About = () => {
+
+  const itemsAbout = [
+    {
+      img: 'url_de_la_imagen',
+      title: 'Título del elemento',
+      text: 'Texto descriptivo del elemento.'
+    },
+    {
+      img: 'url_de_la_imagen',
+      title: 'Título del elemento',
+      text: 'Texto descriptivo del elemento.'
+    },
+    {
+      img: 'url_de_la_imagen',
+      title: 'Título del elemento',
+      text: 'Texto descriptivo del elemento.'
+    },
+  ]
+
   return (
-    <div>about</div>
+    <div className="about-wrapper">
+      <div className="about-container">
+        <div className='top-container'>
+          <p className="title-about">
+            AWS explora 
+          </p>
+        </div>
+        <div className='bot-container'>
+        {itemsAbout.map((item, index) => (
+          <div key={index} className="about-item">
+            <img src={item.img} alt={`Imagen ${index}`} />
+            <p className="item-title">{item.title}</p>
+            <p className="item-text">{item.text}</p>
+          </div>
+        ))}
+        </div>
+      </div>
+    </div>
   )
 }
