7bef94df071aabff7c53ff04d4d3e7644467fa18
Jhamil <jhamil.arroyo.a@gmail.com>
2024-01-11 17:06:15 -0600

    Responsive nav, responsive menu

--- src/components/parallax/Parallax.jsx
+++ src/components/parallax/Parallax.jsx
@@ -1,32 +1,18 @@
-import { motion, useScroll, useTransform } from "framer-motion";
 import "../../styles/parallax.css";
-import { useRef } from "react";
 
 const Parallax = () => {
-  const ref = useRef();
-
-  const { scrollYProgress } = useScroll({
-    target: ref,
-    offset: ["start start", "end start"],
-  });
-
-  const yText = useTransform(scrollYProgress, [0, 1], ["0%", "1500%"]);
-  const yText2 = useTransform(scrollYProgress, [0, 1], ["0%", "2500%"]);
-  const yBg = useTransform(scrollYProgress, [0, 1], ["0%", "130%"]);
 
   return (
     <div className="parallax">
-      <div className="container-texts">
-        <motion.h1 className="special-text" style={{ y: yText }}>
-          AWS para Todos, AWS para Ti
-        </motion.h1>
-        <motion.p className="special-text-2" style={{ y: yText2 }}>
-          Descubre el Poder de AWS
-        </motion.p>
-      </div>
-      <motion.div className="mountains"></motion.div>
-      <motion.div style={{ y: yBg }} className="planets"></motion.div>
-      <motion.div style={{ x: yBg }} className="stars"></motion.div>
+      <div className="mountains"></div>
+        <div className="container-texts">
+          <h1 className="special-text">
+            AWS para Todos, AWS para Ti
+          </h1>
+          <p className="special-text-2">
+            Descubre el Poder de AWS
+          </p>
+        </div>
     </div>
   );
 };
