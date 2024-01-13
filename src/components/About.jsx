e46cd04a1ca897665bbd5be7946470d438968533
Jhamil <jhamil.arroyo.a@gmail.com>
2024-01-13 10:00:51 -0600

    web responsive detalles

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,38 @@
 import '../styles/about-section.css'
+import image from '../images/stars.jpg'
+import { useState } from 'react';
+import { useRef } from 'react';
+import { useEffect } from 'react';
 
 export const About = () => {
+  const [transform, setTransform] = useState({ rotateX: 0, rotateY: 0, scale: 1 });
+  const imagenRef = useRef();
+
+  const handleMouseMove = (e) => {
+    const { clientX, clientY } = e;
+    const { left, top, width, height } = imagenRef.current.getBoundingClientRect();
+
+    const rotateY = ((clientX - left) / width - 0.5) * 20; // Ajusta según tus necesidades
+    const rotateX = -((clientY - top) / height - 0.5) * 20; // Ajusta según tus necesidades
+
+    setTransform({ rotateX, rotateY, scale: 1.1 });
+  };
+
+  const handleMouseLeave = () => {
+    setTransform({ rotateX: 0, rotateY: 0, scale: 1 });
+  };
+
+  useEffect(() => {
+    const imagen = imagenRef.current;
+    imagen.addEventListener('mousemove', handleMouseMove);
+    imagen.addEventListener('mouseleave', handleMouseLeave);
+
+    return () => {
+      imagen.removeEventListener('mousemove', handleMouseMove);
+      imagen.removeEventListener('mouseleave', handleMouseLeave);
+    };
+  }, []);
+  
 
   const itemsAbout = [
     {
@@ -29,6 +61,17 @@ export const About = () => {
           </p>
         </div>
         <div className='bot-container'>
+        <div className="contenedor-3d">
+      <img
+        ref={imagenRef}
+        src={image} // Reemplaza con la ruta real de tu imagen
+        alt="Descripción de la imagen"
+        className="imagen-3d"
+        style={{
+          transform: `rotateX(${transform.rotateX}deg) rotateY(${transform.rotateY}deg)`,
+        }}
+      />
+    </div>
         {itemsAbout.map((item, index) => (
           <div key={index} className="about-item">
             <img src={item.img} alt={`Imagen ${index}`} />
